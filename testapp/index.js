17e9a7f57b7094f08ca6a0c7f846a25366b7df37
uselessgoddess <dodickgod@gmail.com>
2021-12-11 13:38:31 +0300

    Add testapp

---
+++ testapp/index.js
@@ -0,0 +1,19 @@
+import * as wasm from "doublets-web"
+
+const constants = new wasm.LinksConstants();
+let links = new wasm.UnitedLinks(constants);
+
+let link = links.create();
+link = links.update(link, link, link);
+
+console.log("The number of links in the data store is " + links.count() + ".");
+console.log("Data store contents:");
+
+let cont = links.constants._continue;
+let any = links.constants.any;
+links.each(function (link) {
+    console.log(link);
+    return cont;
+}, new wasm.Link(any, any, any));
+
+links.delete(link);
